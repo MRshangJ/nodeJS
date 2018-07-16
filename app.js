d8f6e27ab9da1abd9164ccd39bd3df882316ede2
商健 <1175929873@qq.com>
2018-07-16 21:44:12 +0800

    除首页按钮外全部完成

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path=require("path");
 const session=require("express-session");
 const bodyParser=require("body-parser");
 const MongoClient = require('mongodb').MongoClient;
-
+const tem = require('art-template');    //导入art-template模块
 
  
 let app = express();    //调用方法创建服务器
@@ -66,7 +66,26 @@ app.post("/login",(req,res)=>{
 })
 app.get("/index",(req,res)=>{
    if(req.session.userinfo){
-    res.sendFile(path.join(__dirname,"static/views/index.html"));
+       
+     
+        MongoClient.connect(url, function (err, client) {
+            // Database Name
+            // test-使用库的名字
+            const db = client.db('test');
+            // 链接到集合 hero-使用集合的名字
+            const collection = db.collection('studentInfo');
+            // 精确查找 find({key:val,key1:val1,...}) 不给对象就是找全部
+            collection.find({}).toArray((err, docs) => {
+                console.log(docs);
+                let html = tem(path.join(__dirname,'static/views/index.html'),{    //模板所存放的绝对路径
+                    docs
+                 });
+                 console.log(html);
+                res.send(html);
+               // 关闭链接
+               client.close();
+            });
+        });
    }else{
     res.redirect("/login");  
    }
@@ -90,12 +109,12 @@ MongoClient.connect(url, function (err, client) {
             collection.insertOne({
                 username,userpass
             }, (err, result) => {
-                res.send("<script>alert('注册成功！！！');window.location='/login'</script>");
+                res.send("<script>alert('注册成功！！！');window.location='/index'</script>");
             });
             // 关闭链接
             client.close();
        }else{
-        res.send("<script>alert('账号已存在');window.location='/register'</script>"); 
+        res.send("<script>alert('学生已存在，无法新增');window.location='/index'</script>"); 
        }
     });
    
@@ -107,6 +126,62 @@ app.get("/logout",(req,res)=>{
 })
 app.get("/userinfo",(req,res)=>{
     res.send(req.session.userinfo.username);
+})
+app.get("/addstu",(req,res)=>{
+    res.sendFile(path.join(__dirname,"static/views/update.html"));
+})
+app.post("/addstu",(req,res)=>{
+    console.log(req.body);
+    let name=req.body.name;
+    let age=req.body.age;
+    let sex=req.body.sex;
+    let phone=req.body.phone;
+    let address=req.body.address;
+    let aintroduction=req.body.introduction;
+MongoClient.connect(url, function (err, client) {
+    // Database Name
+    // test-使用库的名字
+    const db = client.db('test');
+    // 链接到集合 hero-使用集合的名字
+    const collection = db.collection('studentInfo');
+    // 精确查找 find({key:val,key1:val1,...}) 不给对象就是找全部
+    collection.find({name}).toArray((err, docs) => {
+       if(docs.length==0){
+            collection.insertOne({
+                name,age,sex,phone,address,aintroduction
+            }, (err, result) => {
+                res.send("<script>alert('新增成功！！！');window.location='/login'</script>");
+            });
+            // 关闭链接
+            client.close();
+       }else{
+        res.send("<script>alert('账号已存在');window.location='/register'</script>"); 
+       }
+    });
+   
+});
+
+
+})
+app.post("/findstu",(req,res)=>{
+    let name=req.body.name;
+    MongoClient.connect(url, function (err, client) {
+        // Database Name
+        // test-使用库的名字
+        const db = client.db('test');
+        // 链接到集合 hero-使用集合的名字
+        const collection = db.collection('studentInfo');
+        // 精确查找 find({key:val,key1:val1,...}) 不给对象就是找全部
+        collection.find({name}).toArray((err, docs) => {
+            let html = tem(path.join(__dirname,'static/views/index.html'),{    //模板所存放的绝对路径
+                docs
+             });
+             console.log(html);
+            res.send(html);
+           // 关闭链接
+           client.close();
+        });
+    });
 })
 app.listen(80,'127.0.0.1',()=>{console.log('success')});    //监听127.0.0.1:80
 //请使用命令 npm i express 将express模块done下来；
